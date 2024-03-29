8c13bd4035ce56b12277647e97dd21df2c640dac
Ujjwal Joshi <ujjwaljoshi93@gmail.com>
2024-03-29 18:12:44 -0400

    changes

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
 app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/api/getAsset/:fileName', (req, res) => {
